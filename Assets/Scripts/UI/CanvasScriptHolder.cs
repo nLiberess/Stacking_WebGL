c918a75df1436edbc3387674ce08cc83cff90a1b
nLiberess <145514586+nLiberess@users.noreply.github.com>
2024-03-08 17:24:36 +0900

    CHG: UIMgr, RankPanel

--- Assets/Scripts/UI/CanvasScriptHolder.cs
+++ Assets/Scripts/UI/CanvasScriptHolder.cs
@@ -4,27 +4,28 @@ using UnityEngine;
 
 public class CanvasScriptHolder : MonoBehaviour
 {
-    public GameObject ToggleOn, ToggleOff;
-    public GameObject InfoPopUp;
+    public GameObject toggleOn, toggleOff;
+    public GameObject infoPopUp;
 
     public void GetCI() => Application.OpenURL("https://2h1z.app.link/aisoU3nX4zb");
-    public void SoundCheck(bool B)
+    
+    public void SoundCheck(bool active)
     {
-        ToggleOn.SetActive(false);
-        ToggleOff.SetActive(false);
-        if(B)
+        toggleOn.SetActive(false);
+        toggleOff.SetActive(false);
+        if(active)
         {
-            ToggleOn.SetActive(true);
+            toggleOn.SetActive(true);
             SoundManager.Inst.SetSfxVolume(1.0f);
         }
-        else if(!B)
+        else if(!active)
         {
-            ToggleOff.SetActive(true);
+            toggleOff.SetActive(true);
             SoundManager.Inst.SetSfxVolume(0.0f);
         }
     }
 
-    public void InfoGet(bool B) => InfoPopUp.SetActive(B);
+    public void InfoGet(bool active) => infoPopUp.SetActive(active);
 
-    public void rankPanel(bool B) => UIManager.Inst.HandleRankPanel(B);
+    public void HandleRankPanel(bool active) => UIManager.Inst.HandleRankPanel(active);
 }
